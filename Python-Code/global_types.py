7ee2741ef6bb174cd0f70e94d48dae0002cd6b75
tccoulte <tccoulte@buffalo.edu>
2016-04-18 11:37:58 -0400

    Update global_types.py

--- Python-Code/global_types.py
+++ Python-Code/global_types.py
@@ -2,7 +2,7 @@ from enum import enum
 
 global class_state = enum('TIMEOUT','RECEIVED','TRANSMITTED','KILLED','IDLE','NOT_RECEIVED')
 
-global phone_state = enum()
+global phone_state = enum('INCREMENT_AND_WAIT','CHECK_AND_WAIT','REQUEST_AND_WAIT','ERROR_CHECK','ENCRYPT_AND_WAIT')
 
 global station_state = enum()
 
